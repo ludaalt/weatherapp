5fe4e9c630e06666c1e811dd9adf4678412fb43a
Людмила Алтухова <luda.tamb@mail.ru>
2023-06-08 15:12:15 +0300

    refactor: correct history component

--- src/components/RequestsHistory.tsx
+++ src/components/RequestsHistory.tsx
@@ -4,28 +4,17 @@ import styled from "styled-components";
 
 import WeatherData from "../store/weatherData";
 
-const HistoryList = styled.ul`
-  width: 200px;
-  height: 200px;
-  font-size: 20px;
+const History = styled.div`
+  font-size: 30px;
 
   li {
     margin-top: 10px;
-
-    a {
-      color: #fff;
-      text-decoration: none;
-
-      &:hover {
-        text-decoration: underline;
-      }
-    }
   }
 `;
 
 const RequestsHistory = observer(() => {
   const changeCurrentCity = (city: string) => {
-    if (WeatherData.currentCity && WeatherData.currentCity.name !== city) {
+    if (WeatherData.currentCity!.name !== city) {
       for (let i = 0; i < WeatherData.data.length; i += 1) {
         if (WeatherData.data[i].name === city) {
           WeatherData.currentCity = WeatherData.data[i];
@@ -35,18 +24,16 @@ const RequestsHistory = observer(() => {
   };
 
   return (
-    <div>
+    <History>
       <h2>History</h2>
-      <HistoryList>
-        {WeatherData.historyOfCities &&
-          WeatherData.historyOfCities.length > 0 &&
-          WeatherData.historyOfCities.map((item, index) => (
-            <li key={index} onClick={() => changeCurrentCity(item)}>
-              <Link to="/weather">{item}</Link>
-            </li>
-          ))}
-      </HistoryList>
-    </div>
+      <ul>
+        {WeatherData.historyOfCities!.map((item) => (
+          <li key={item} onClick={() => changeCurrentCity(item)}>
+            <Link to="/weather">{item}</Link>
+          </li>
+        ))}
+      </ul>
+    </History>
   );
 });
 
